7abca88cee2fa433b28afda5a0c847cb2b592bf1
Cppshizoid <alex.krivoschecov@gmail.com>
2023-10-21 06:39:20 +0700

    Update Shizmatrix.hpp

--- Matrix_lib/Shizmatrix.hpp
+++ Matrix_lib/Shizmatrix.hpp
@@ -82,3 +82,133 @@ public:
     return 0;
   }
 };
+
+template <typename T>
+class matrix : public expression<matrix<T>> {
+private:
+
+  std::vector<T> container;
+
+  std::size_t n_rows;
+
+  std::size_t n_cols;
+
+public:
+
+  using EvalReturnType = matrix<T>;
+
+  using ElementType = T;
+
+  inline matrix() = default;
+
+  inline matrix(std::size_t n_rows, std::size_t n_cols)
+      : container(n_rows * n_cols), n_rows(n_rows), n_cols(n_cols) {}
+
+  inline matrix(std::size_t n_rows, std::size_t n_cols, T fill)
+      : container(n_rows * n_cols, fill), n_rows(n_rows), n_cols(n_cols) {}
+
+  inline matrix(matrix<T> &&other)
+      : container(std::move(other.container)), n_rows(other.num_rows()), n_cols(other.num_cols()) {}
+
+  template <typename E>
+  inline matrix(expression<E> const &other)
+      : container(other.num_rows() * other.num_cols()), n_rows(other.num_rows()),
+        n_cols(other.num_cols()) {
+    assign(other.get_const_derived());
+  }
+
+  template <typename E>
+  inline matrix &operator=(expression<E> const &other) {
+    assert(n_rows >= other.num_rows());
+    assert(n_cols >= other.num_cols());
+    assign(other.get_const_derived());
+    return *this;
+  }
+
+  inline matrix &operator=(matrix<T> &&other) {
+    if (this != &other) {
+      container = std::move(other.container);
+      n_rows = other.n_rows;
+      n_cols = other.n_cols;
+    }
+    return *this;
+  }
+
+  inline T operator()(std::size_t i, std::size_t j) const {
+    assert(i < n_rows && i >= 0);
+    assert(j < n_cols && j >= 0);
+    return container[i * n_cols + j];
+  }
+
+  inline std::vector<T> &get_container() {
+    return container;
+  }
+
+  inline std::size_t num_rows() const {
+    return n_rows;
+  }
+
+  inline std::size_t num_cols() const {
+    return n_cols;
+  }
+
+  inline const matrix<T> &eval() const {
+    return *this;
+  }
+
+  inline void set_elt(std::size_t i, std::size_t j, T value) {
+    container[i * n_cols + j] = value;
+  }
+
+  template <typename E>
+  inline void assign(expression<E> const &expr) {
+    for (std::size_t i = 0; i < n_rows; ++i) {
+      for (std::size_t j = 0; j < n_cols; ++j) {
+        container[i * n_cols + j] = expr.get_const_derived()(i, j);
+      }
+    }
+  }
+
+  friend std::ostream &operator<<(std::ostream &ostream, const matrix<T> &mat) {
+    for (std::size_t i = 0; i < mat.n_rows; ++i) {
+      for (std::size_t j = 0; j < mat.n_cols; ++j) {
+        ostream << mat(i, j) << ", ";
+      }
+      ostream << "\n";
+    }
+    return ostream;
+  }
+
+  template <typename E>
+  inline matrix<T> &operator+=(expression<E> const &expr);
+
+  template <typename E>
+  inline matrix<T> &operator*=(expression<E> const &expr);
+
+  template <typename E>
+  inline matrix<T> &operator-=(expression<E> const &expr);
+
+  template <typename Scalar, typename = enable_if_not_expression<Scalar>>
+  inline matrix<T> &operator+=(Scalar const &expr);
+
+  template <typename Scalar, typename = enable_if_not_expression<Scalar>>
+  inline matrix<T> &operator-=(Scalar const &expr);
+
+  template <typename Scalar, typename = enable_if_not_expression<Scalar>>
+  inline matrix<T> &operator*=(Scalar const &expr);
+};
+} 
+
+namespace std {
+using namespace shizmatrix;
+
+template <typename T1, typename T2>
+struct common_type<matrix<T1>, matrix<T2>> {
+  using type = matrix<std::common_type_t<T1, T2>>;
+};
+
+template <typename T1, typename T2>
+struct common_type<matrix<T1>, T2> {
+  using type = matrix<std::common_type_t<T1, T2>>;
+};
+} 
