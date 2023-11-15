00980a80270ce6795ddb91e800d1fe2757649e94
Cppshizoid <alex.krivoschecov@gmail.com>
2023-11-15 20:40:37 +0700

    Create main.cpp

---
+++ Power/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <utility>
+//compile time power 
+template <class T, T V, T N, class I = std::make_integer_sequence<T, N>>
+struct power;
+
+template <class T, T V, T N, T... Is>
+struct power<T, V, N, std::integer_sequence<T, Is...>> {
+  static constexpr T value =
+      (static_cast<T>(1) * ... * (V * static_cast<bool>(Is + 1)));
+};
+
+int main() { std::cout << power<int, 4, 2>::value << std::endl; }
