44712876085019217b0eedb5cfa584f13b39626b
Cppshizoid <alex.krivoschecov@gmail.com>
2024-01-15 08:52:58 +0700

    Create count_if.hpp

---
+++ Count_If/count_if.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstddef>
+#include <tuple>
+#include <utility>
+
+namespace cppshizoid {
+template <typename... Ts>
+constexpr auto count_if(auto callable, std::tuple<Ts...> &tuple) {
+  return [&]<std::size_t... indexes>(std::index_sequence<indexes...>) {
+    return (callable(std::get<indexes>(tuple)) + ...);
+  }(std::make_index_sequence<sizeof...(Ts)>{});
+}
+
+constexpr auto count_if(auto callable, auto... Ts) {
+  return (callable(Ts) + ...);
+}
+} // namespace cppshizoid
