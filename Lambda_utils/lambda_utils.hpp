c983cb0e1ee7cb84663254f44253f3113117e377
Cppshizoid <alex.krivoschecov@gmail.com>
2024-01-15 10:19:24 +0700

    Update lambda_utils.hpp

--- Lambda_utils/lambda_utils.hpp
+++ Lambda_utils/lambda_utils.hpp
@@ -1,41 +1,57 @@
-#ifndef LAMBDA_HPP
-#define LAMBDA_HPP
+#ifndef LAMBDA_COROUTINES_HPP
+#define LAMBDA_COROUTINES_HPP
 
 #include <cstddef>
 #include <functional>
 #include <optional>
-#include <string>
-#include <type_traits>
 #include <utility>
 
-namespace cppshizoid::lambda {
+namespace cppshizoid::lambda_coroutines {
 
-constexpr std::string lambda_concatenate_impl(const std::string &a,
-                                                 const std::string &b) {
-  return a + b;
-}
-
-constexpr auto lambda_concatenate(const std::string &a,
-                                     const std::string &b) {
-  return lambda_concatenate_impl(a, b);
-}
-
-constexpr auto lambda_concatenate_line() {
-  return lambda_concatenate("long", "");
-}
+//  tool for checking lambda coroutine requirements
 template <typename ParamType>
-constexpr void lambda_status_check(ParamType &value) {
-  static_assert(std::is_integral_v<ParamType>, "integral state is required");
+constexpr void lambda_co_status_check(ParamType &value) {
+  static_assert(std::is_integral_v<ParamType>, "integral state value required");
   static_assert(!std::is_const_v<ParamType>, "mutable lambda required");
-  static_assert(sizeof(value) >= 4, "state must be at least 32 bits");
+  static_assert(sizeof(value) >= 4, "state value must be at least 32 bits");
 }
 
-inline constexpr std::size_t lambdaconstexpr_line = 0;
+template <typename StateType>
+[[nodiscard]] constexpr auto lambda_co_begin(StateType &state_variable) {
+  lambda_co_status_check(state_variable);
+
+  struct LambdaCoBeginResult {
+    StateType &state_ref;
+    bool is_default_case;
+  };
+
+  return LambdaCoBeginResult{state_variable, false};
+}
+
+template <typename StateType>
+[[nodiscard]] constexpr auto lambda_co_yield(StateType &state_variable) {
+  state_variable = __LINE__;
+  return state_variable;
+}
+
+template <typename ReturnType>
+constexpr auto lambda_co_return(ReturnType return_value) {
+  return return_value;
+}
+
+constexpr void lambda_co_end() {}
+
+// constexpr function for concatenating two lambdas
+template <typename Lambda1, typename Lambda2>
+constexpr auto lambda_co_concatenate(Lambda1 lambda1, Lambda2 lambda2) {
+  return [lambda1, lambda2](auto... args) {
+    lambda1(args...);
+    lambda2(args...);
+  };
+}
 
 template <typename Lambda>
-constexpr auto lambda_range(Lambda lambda, std::size_t skip = 0,
-                               std::optional<std::size_t> length = {},
-                               std::size_t stride = 1) {
+[[nodiscard]] constexpr auto range(Lambda lambda, std::size_t skip_ = 0, std::optional<std::size_t> length_ = {}, std::size_t stride_ = 1) {
   struct Range {
     Lambda generator;
     std::optional<std::size_t> length;
@@ -46,13 +62,14 @@ constexpr auto lambda_range(Lambda lambda, std::size_t skip = 0,
       std::reference_wrapper<Lambda> lambda;
       std::reference_wrapper<std::size_t> position;
       std::optional<std::size_t> length;
-      std::reference_wrapper<std::size_t> stride_ref;
+      std::size_t stride = 1;
       bool is_end = false;
 
-      constexpr bool has_more() const noexcept {
+      [[nodiscard]] constexpr bool has_more() const noexcept {
         if (length && (position.get() >= length.value())) {
           return false;
         }
+
         return true;
       }
 
@@ -66,7 +83,7 @@ constexpr auto lambda_range(Lambda lambda, std::size_t skip = 0,
       }
 
       constexpr auto &operator++() {
-        for (std::size_t i = 1; i < stride_ref.get(); ++i) {
+        for (std::size_t i = 1; i < stride; ++i) {
           if (increment_position()) {
             lambda();
           }
@@ -78,12 +95,13 @@ constexpr auto lambda_range(Lambda lambda, std::size_t skip = 0,
         if (is_end && !rhs.has_more()) {
           return true;
         }
-        if (rhs.is_end && !rhs.has_more()) {
+        if (rhs.is_end && !has_more()) {
           return true;
         }
         if (rhs.is_end && is_end) {
           return true;
         }
+
         return false;
       }
 
@@ -91,28 +109,29 @@ constexpr auto lambda_range(Lambda lambda, std::size_t skip = 0,
         return !(*this == rhs);
       }
 
-      constexpr decltype(auto) operator*() {
+      [[nodiscard]] constexpr decltype(auto) operator*() {
         increment_position();
         return lambda();
       }
     };
 
-    constexpr auto begin() noexcept {
+    [[nodiscard]] constexpr auto begin() noexcept {
       return Iterator{generator, position, length, stride, false};
     }
 
-    constexpr auto end() noexcept {
+    [[nodiscard]] constexpr auto end() noexcept {
       return Iterator{generator, position, length, stride, true};
     }
   };
 
-  for (std::size_t i = 0; i < skip; ++i) {
+  for (std::size_t i = 0; i < skip_; ++i) {
     lambda();
   }
-  return Range{std::move(lambda), length, stride};
+  return Range{std::move(lambda), length_, stride_};
 }
 
-template <typename Lambda> constexpr auto while_has_value(Lambda lambda) {
+template <typename Lambda>
+[[nodiscard]] constexpr auto while_has_value(Lambda lambda) {
   struct Range {
     Lambda generator;
 
@@ -121,9 +140,10 @@ template <typename Lambda> constexpr auto while_has_value(Lambda lambda) {
       std::invoke_result_t<Lambda> current_value{};
 
       Iterator(Lambda &lambda_, bool is_end_)
-          : lambda(lambda_), current_value([&]() {
-              return is_end_ ? decltype(current_value){} : lambda();
-            }()) {}
+        : lambda(lambda_), current_value([&]() {
+          return is_end_ ? decltype(current_value){} : lambda();
+        }()) {
+      }
 
       constexpr auto &operator++() {
         current_value = lambda();
@@ -134,6 +154,7 @@ template <typename Lambda> constexpr auto while_has_value(Lambda lambda) {
         if (!current_value && !rhs.current_value) {
           return true;
         }
+
         return false;
       }
 
@@ -141,16 +162,23 @@ template <typename Lambda> constexpr auto while_has_value(Lambda lambda) {
         return !(*this == rhs);
       }
 
-      constexpr decltype(auto) operator*() { return current_value.value(); }
+      [[nodiscard]] constexpr decltype(auto) operator*() {
+        return current_value.value();
+      }
     };
 
-    constexpr auto begin() noexcept { return Iterator{generator, false}; }
+    [[nodiscard]] constexpr auto begin() noexcept {
+      return Iterator{generator, false};
+    }
 
-    constexpr auto end() noexcept { return Iterator{generator, true}; }
+    [[nodiscard]] constexpr auto end() noexcept {
+      return Iterator{generator, true};
+    }
   };
 
   return Range{std::move(lambda)};
 }
-} // namespace cppshizoid::lambda
 
-#endif // !LAMBDA_HPP
+}  // namespace cppshizoid::lambda_coroutines
+
+#endif
