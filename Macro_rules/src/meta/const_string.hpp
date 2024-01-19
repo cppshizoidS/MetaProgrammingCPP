a84933014e2b1a6227522e6d7dc05624df4e2b55
Cppshizoid <alex.krivoschecov@gmail.com>
2024-01-19 14:24:12 +0700

    Create const_string.hpp

---
+++ Macro_rules/src/meta/const_string.hpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <string_view>
+
+template <size_t N> struct const_string : std::array<char, N> {
+  constexpr const_string(const char (&chars)[N]) {
+    std::copy_n(chars, N, std::array<char, N>::begin());
+  }
+
+  [[nodiscard]] constexpr auto str() const -> std::string_view {
+    return std::string_view(std::array<char, N>::data(), N - 1);
+  }
+};
+
+template <size_t N> const_string(const char (&)[N]) -> const_string<N>;
+
+static constexpr auto fnv1a(std::string_view s) -> uint32_t {
+  uint32_t hash = 0;
+  for (char i : s) {
+    hash *= 0x811C9DC5;
+    hash ^= static_cast<uint32_t>(i);
+  }
+  return hash;
+}
