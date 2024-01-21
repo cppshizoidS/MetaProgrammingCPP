28d4bc4f9804a28cda315cf46aacc91e419fee5d
Cppshizoid <alex.krivoschecov@gmail.com>
2024-01-22 05:49:23 +0700

    Update main.cpp
    add better example

--- Constructable_With/main.cpp
+++ Constructable_With/main.cpp
@@ -1,32 +1,33 @@
-#include "constructable_with.hpp"
 #include <iostream>
-struct Person {
-  std::string name;
-  int age;
+#include "constructable_with.hpp"
+
+struct Point2D {
+  int x, y;
 };
 
-struct Book {
-  std::string title;
-  std::string author;
-  int year;
+struct ColorRGB {
+  int red, green, blue;
+};
+
+struct InvalidType {
+  // This type has more than 12 members, so it doesn't meet the concept requirements.
+  int a, b, c, d, e, f, g, h, i, j, k, l, m;
 };
 
 int main() {
-  // is person is aggrefate of
-  if constexpr (lib::aggregate_of<Person,
-                                  lib::number_of_aggregate_members<Person>>) {
-    std::cout << "Struct Person is aggregate of" << std::endl;
-  } else {
-    std::cout << "Struct Person is not aggregate of" << std::endl;
-  }
+  constexpr bool isPoint2DAggregate = lib::aggregate_of<Point2D, 2>;  // Should be true
+  constexpr bool isColorRGBAggregate = lib::aggregate_of<ColorRGB, 3>;  // Should be true
+  constexpr bool isInvalidAggregate = lib::aggregate_of<InvalidType, 12>;  // Should be false
 
-  // is boook is aggrefate of
-  if constexpr (lib::aggregate_of<Book,
-                                  lib::number_of_aggregate_members<Book>>) {
-    std::cout << "Struct Book is aggregate of" << std::endl;
-  } else {
-    std::cout << "Struct Book is not aggregate of" << std::endl;
-  }
+  std::cout << "Point2D is an aggregate: " << isPoint2DAggregate << std::endl;
+  std::cout << "ColorRGB is an aggregate: " << isColorRGBAggregate << std::endl;
+  std::cout << "InvalidType is an aggregate: " << isInvalidAggregate << std::endl;
+
+  constexpr auto numMembersPoint2D = lib::number_of_aggregate_members<Point2D>;
+  constexpr auto numMembersColorRGB = lib::number_of_aggregate_members<ColorRGB>;
+
+  std::cout << "Number of members in Point2D: " << numMembersPoint2D << std::endl;
+  std::cout << "Number of members in ColorRGB: " << numMembersColorRGB << std::endl;
 
   return 0;
 }
