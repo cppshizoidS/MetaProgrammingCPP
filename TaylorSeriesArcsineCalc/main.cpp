f9fd4ead6d769d6a1afbfaa2118a8760abb77cc9
Cppshizoid <alex.krivoschecov@gmail.com>
2024-01-24 07:04:36 +0700

    Update main.cpp

--- TaylorSeriesArcsineCalc/main.cpp
+++ TaylorSeriesArcsineCalc/main.cpp
@@ -1,19 +1,20 @@
 #include "asin.hpp"
 #include <numbers>
-#include <iostream>
+auto main() -> int {
+  constexpr double x = 0.5;
+  constexpr int terms = 10;
 
-int main() {
-    constexpr double x = 0.5;
-    constexpr int terms = 10;
+  constexpr double asinApprox = taylorSeriesASin(x, terms);
+  constexpr double asinDegrees = asinApprox * 180 / std::numbers::pi;
 
-    constexpr double asinApprox = taylorSeriesASin(x, terms);
-    constexpr double asinDegrees = asinApprox * 180.0 / std::numbers::pi;
+  constexpr double chebasinApprox = chebyshevASin(x);
+  double chebasinDegrees = std::asin(x) * 180.0 / std::numbers::pi;
 
-    static_assert(asinDegrees >= 25.999999999 && asinDegrees <= 30.000000001, "Incorrect approximation of asin(0.5)");
+  printf("Taylor Approximation of asin(0.5): %f\n", asinApprox);
+  printf("Asin(0.5) in degrees: %f\n", asinDegrees);
+  static_assert(checkAsinRange(), "Incorrect approximation of asin(0.5)");
 
-    std::cout << "Approximation of asin(" << x << ") using " << terms << " terms: " << asinDegrees << " degrees" << std::endl;
+  printf("Chebash Approximation of asin(0.5): %f\n", chebasinApprox);
+  printf("Asin(0.5) in degrees: %f\n", chebasinDegrees);
 
-
-    return 0;
 }
-
