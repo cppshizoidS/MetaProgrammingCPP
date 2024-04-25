d7f38762f5f7d38ea91b6f765a8d52c02c19de46
cppshizoid <alex.krivoschecov@gmail.com>
2024-04-26 05:12:46 +0700

    somerefactor

--- Variant/main.cpp
+++ Variant/main.cpp
@@ -3,53 +3,61 @@
 #include <print>
 #include <type_traits>
 
-template <typename Head, typename... Tail> struct MaxSizeHelper {
-  static constexpr size_t MaxSize() {
+template <typename Head, typename... Tail>
+struct MaxSizeHelper {
+  static constexpr std::size_t MaxSize() {
     return std::max(sizeof(Head), MaxSizeHelper<Tail...>::MaxSize());
   }
 };
 
-template <typename Head> struct MaxSizeHelper<Head> {
-  static constexpr size_t MaxSize() { return sizeof(Head); }
+template <typename Head>
+struct MaxSizeHelper<Head> {
+  static constexpr std::size_t MaxSize() { return sizeof(Head); }
 };
 
-template <typename... Types> constexpr size_t MaxSize() {
+template <typename... Types>
+constexpr std::size_t MaxSize() {
   return MaxSizeHelper<Types...>::MaxSize();
 }
 
-template <typename Head, typename... Tail> struct MaxAlignmentHelper {
-  static constexpr size_t MaxAlignment() {
+template <typename Head, typename... Tail>
+struct MaxAlignmentHelper {
+  static constexpr std::size_t MaxAlignment() {
     return std::max(alignof(Head), MaxAlignmentHelper<Tail...>::MaxAlignment());
   }
 };
 
-template <typename Head> struct MaxAlignmentHelper<Head> {
-  static constexpr size_t MaxAlignment() { return alignof(Head); }
+template <typename Head>
+struct MaxAlignmentHelper<Head> {
+  static constexpr std::size_t MaxAlignment() { return alignof(Head); }
 };
 
-template <typename... Types> constexpr size_t MaxAlignment() {
+template <typename... Types>
+constexpr std::size_t MaxAlignment() {
   return MaxAlignmentHelper<Types...>::MaxAlignment();
 }
 
 template <typename Needle, typename HayStackHead, typename... HayStackTail>
 struct FindHelper {
-  static constexpr size_t Find() {
+  static constexpr std::size_t Find() {
     return 1 + FindHelper<Needle, HayStackTail...>::Find();
   }
 };
 
 template <typename Needle, typename... HayStackTail>
 struct FindHelper<Needle, Needle, HayStackTail...> {
-  static constexpr size_t Find() { return 0; }
+  static constexpr std::size_t Find() { return 0; }
 };
 
-template <typename Needle, typename... HayStack> constexpr size_t Find() {
+template <typename Needle, typename... HayStack>
+constexpr std::size_t Find() {
   return FindHelper<Needle, HayStack...>::Find();
 }
 
-template <size_t I, typename... Types> struct ByIndex {};
+template <std::size_t I, typename... Types>
+struct ByIndex {};
 
-template <size_t I, typename TypesHead, typename... TypesTail>
+template <std::size_t I, typename TypesHead, typename... TypesTail>
 struct ByIndex<I, TypesHead, TypesTail...> {
   using Type = typename ByIndex<I - 1, TypesTail...>::Type;
 };
@@ -59,15 +67,17 @@ struct ByIndex<0, TypesHead, TypesTail...> {
   using Type = TypesHead;
 };
 
-template <typename T> constexpr void Destroy(void *ptr) {
+template <typename T>
+constexpr void Destroy(void *ptr) {
   std::println("{}", __PRETTY_FUNCTION__);
   // std::cout << __PRETTY_FUNCTION__ << std::endl;
   reinterpret_cast<T *>(ptr)->~T();
 }
 
-template <typename... T> struct helper {};
+template <typename... T>
+struct helper {};
 
-template <typename... Types, size_t... I>
+template <typename... Types, std::size_t... I>
 struct helper<std::tuple<Types...>, std::index_sequence<I...>> {
   static constexpr auto make_arr() {
     return std::array<void (*)(void *), sizeof...(I)>{
@@ -76,15 +86,16 @@ struct helper<std::tuple<Types...>, std::index_sequence<I...>> {
 };
 
 template <typename... Types>
-constexpr void Destroy(size_t typeIndex, void *ptr) {
+constexpr void Destroy(std::size_t typeIndex, void *ptr) {
   static constexpr std::array<void (*)(void *), sizeof...(Types)> arr =
       helper<std::tuple<Types...>,
              std::make_index_sequence<sizeof...(Types)>>::make_arr();
   arr[typeIndex](ptr);
 }
 
-template <typename... Types> class Variant {
-public:
+template <typename... Types>
+class Variant {
+ public:
   template <typename Type>
   constexpr explicit Variant(Type &&value)
       : m_typeIndex(
@@ -94,7 +105,8 @@ public:
 
   constexpr ~Variant() noexcept { Destroy<Types...>(m_typeIndex, m_storage); }
 
-  template <typename Type> constexpr Variant &operator=(Type &&value) {
+  template <typename Type>
+  constexpr Variant &operator=(Type &&value) {
     this->~Variant();
     new (this) Variant{std::forward<Type>(value)};
   }
@@ -106,12 +118,13 @@ public:
     new (m_storage) Type{std::forward<TArgs>(args)...};
   }
 
-private:
+ private:
   alignas(MaxAlignment<Types...>()) uint8_t m_storage[MaxSize<Types...>()];
   uint8_t m_typeIndex;
 };
 
-struct Log {
+class Log {
+ public:
   Log() { std::print("Log()\n"); }
   Log(const Log &other) { std::print("Log(const Log& other)\n"); }
   Log(Log &&other) noexcept { std::print("Log(Log&& other)\n"); }
@@ -125,7 +138,7 @@ struct Log {
   }
   ~Log() noexcept { std::print("~Log()\n"); }
 
-private:
+ private:
   alignas(64) uint8_t buffer[155];
 };
 
