f8347a4a494d97416cb709089f8b5e7245d7e4d3
Cppshizoid <alex.krivoschecov@gmail.com>
2024-05-30 00:18:02 +0700

    Add files via upload

---
+++ To_Tuple/main.cpp
@@ -0,0 +1,52 @@
+#include <print>
+#include <tuple>
+#include <utility>
+
+/**
+ * @brief Converts an objects to a tuple if possible
+ *
+ * Function attemps to decomposite object into a tuple using structured
+ * bindings. If the object can be decomposited into two elements, it returns  a
+ * tuple containing this elemetns. Otherwise, it triggers a static assestion
+ * failure.
+ *
+ * @tparam T The type of the object to be converted to a tuple
+ * @param obj The object to be converted to a tuple
+ * @return A tuple containing the decomposed elements of the object
+ * */
+
+template <class T>
+[[nodiscard]] constexpr auto to_tuple(T&& obj) {
+  if constexpr (requires {
+                  [&obj]() -> decltype(auto) {
+                    auto&& [p1, p2] = obj;
+                    return std::tuple{p1, p2};
+                  };
+                }) {
+    auto&& [p1, p2] = std::forward<T>(obj);
+    return std::tuple{p1, p2};
+  } else {
+    struct foo {
+      int i{};
+      bool b{};
+    };
+    static_assert(std::tuple{42, true} == to_tuple(foo{.i = 42, .b = true}),
+                  "Not equal");
+  }
+}
+/**
+ * @brief Structure with integer and double
+ * */
+struct MyStruct {
+  int a;
+  double b;
+};
+
+int main() {
+  constexpr MyStruct s{1, 2.0};
+  auto t = to_tuple(s);
+
+  std::print("Tuple elements: ({}, {})\n", std::get<0>(t), std::get<1>(t));
+
+  return 0;
+}
