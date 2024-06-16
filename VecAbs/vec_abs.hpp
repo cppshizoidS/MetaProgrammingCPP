0fcd0fcc89e8ab133b62786a788bd98a573a8d89
Cppshizoid <alex.krivoschecov@gmail.com>
2024-06-17 01:56:11 +0700

    Update vec_abs.hpp

--- VecAbs/vec_abs.hpp
+++ VecAbs/vec_abs.hpp
@@ -3,73 +3,75 @@
 
 #include <array>
 #include <cstddef>
+#include <print>
 #include <type_traits>
 #include <utility>
-#include <print>
 
 namespace vec {
-    template<typename T, typename... Ts>
-    constexpr bool all_convertible_to() {
-        return (std::is_convertible_v<T, Ts> && ...);
-    }
+template <typename T, typename... Ts>
+constexpr bool all_convertible_to() {
+  return (std::is_convertible_v<T, Ts> && ...);
+}
 
-    template<typename T, size_t N>
-    class vec_abs {
-    public:
-        consteval vec_abs();
+template <typename T, size_t N>
+class vec_abs {
+ public:
+  consteval vec_abs();
 
-        [[maybe_unused]] constexpr vec_abs(const vec_abs &vec);
+  [[maybe_unused]] constexpr vec_abs(const vec_abs &vec);
 
-        [[maybe_unused]] constexpr vec_abs(vec_abs &&vec) noexcept;
+  [[maybe_unused]] constexpr vec_abs(vec_abs &&vec) noexcept;
 
-        [[maybe_unused]] explicit constexpr vec_abs(const std::array<T, N> &arr);
+  [[maybe_unused]] explicit constexpr vec_abs(const std::array<T, N> &arr);
 
-        template<typename... Ts>
-        requires std::conjunction_v<std::is_convertible<Ts, T>...> &&
-                 (sizeof...(Ts) == N)
-        explicit consteval vec_abs(Ts &&... values);
-
-        constexpr vec_abs &operator=(const vec_abs &other);
-
-        constexpr vec_abs &operator=(vec_abs &&other) noexcept;
-
-        std::array<T, N> data;
-    };
-
-    template<typename T, size_t N>
-    consteval vec_abs<T, N>::vec_abs() : data{} {}
-
-    template<typename T, size_t N>
-    [[maybe_unused]] constexpr vec_abs<T, N>::vec_abs(const vec_abs &vec) : data(vec.data) {}
-
-    template<typename T, size_t N>
-    [[maybe_unused]] constexpr vec_abs<T, N>::vec_abs(vec_abs &&vec) noexcept
-            : data(std::move(vec.data)) {}
-
-    template<typename T, size_t N>
-    [[maybe_unused]] constexpr vec_abs<T, N>::vec_abs(const std::array<T, N> &arr) : data(arr) {}
-
-    template<typename T, size_t N>
-    template<typename... Ts>
+  template <typename... Ts>
     requires std::conjunction_v<std::is_convertible<Ts, T>...> &&
              (sizeof...(Ts) == N)
-    consteval vec_abs<T, N>::vec_abs(Ts &&... values)
-            : data{std::forward<Ts>(values)...} {}
+  explicit consteval vec_abs(Ts &&...values);
 
-    template<typename T, size_t N>
-    constexpr vec_abs<T, N> &vec_abs<T, N>::operator=(const vec_abs &other) {
-        if (this != &other) {
-            data = other.data;
-        }
-        return *this;
-    }
+  constexpr vec_abs &operator=(const vec_abs &other);
 
-    template<typename T, size_t N>
-    constexpr vec_abs<T, N> &vec_abs<T, N>::operator=(vec_abs &&other) noexcept {
-        if (this != &other) {
-            data = std::move(other.data);
-        }
-        return *this;
-    }
+  constexpr vec_abs &operator=(vec_abs &&other) noexcept;
+
+  std::array<T, N> data;
+};
+
+template <typename T, size_t N>
+consteval vec_abs<T, N>::vec_abs() : data{} {}
+
+template <typename T, size_t N>
+[[maybe_unused]] constexpr vec_abs<T, N>::vec_abs(const vec_abs &vec)
+    : data(vec.data) {}
+
+template <typename T, size_t N>
+[[maybe_unused]] constexpr vec_abs<T, N>::vec_abs(vec_abs &&vec) noexcept
+    : data(std::move(vec.data)) {}
+
+template <typename T, size_t N>
+[[maybe_unused]] constexpr vec_abs<T, N>::vec_abs(const std::array<T, N> &arr)
+    : data(arr) {}
+
+template <typename T, size_t N>
+template <typename... Ts>
+  requires std::conjunction_v<std::is_convertible<Ts, T>...> &&
+           (sizeof...(Ts) == N)
+consteval vec_abs<T, N>::vec_abs(Ts &&...values)
+    : data{std::forward<Ts>(values)...} {}
+
+template <typename T, size_t N>
+constexpr vec_abs<T, N> &vec_abs<T, N>::operator=(const vec_abs &other) {
+  if (this != &other) {
+    data = other.data;
+  }
+  return *this;
 }
-#endif // VEC_ABS
+
+template <typename T, size_t N>
+constexpr vec_abs<T, N> &vec_abs<T, N>::operator=(vec_abs &&other) noexcept {
+  if (this != &other) {
+    data = std::move(other.data);
+  }
+  return *this;
+}
+}  // namespace vec
+#endif  // VEC_ABS
